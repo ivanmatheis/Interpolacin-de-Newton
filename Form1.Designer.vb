f2340d69d88b1885794424de047d97c3a5e2bfe1
Ivan Paz Matheis <ivan.matheis14@gmail.com>
2022-05-09 21:02:27 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,370 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tn = New System.Windows.Forms.TextBox()
+        Me.tc = New System.Windows.Forms.TextBox()
+        Me.tx = New System.Windows.Forms.TextBox()
+        Me.tin = New System.Windows.Forms.TextBox()
+        Me.ibb = New System.Windows.Forms.TextBox()
+        Me.Salida = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column11 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Calcular = New System.Windows.Forms.Button()
+        Me.Graficar = New System.Windows.Forms.Button()
+        Me.Limpiar = New System.Windows.Forms.Button()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ts = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.tm = New System.Windows.Forms.TextBox()
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(80, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Dame numero de parejas"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(80, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Dame valor a interpolar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(80, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Dame cifras significativas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(131, 159)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "ib"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(108, 242)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Interpolacion"
+        '
+        'tn
+        '
+        Me.tn.Location = New System.Drawing.Point(264, 46)
+        Me.tn.Name = "tn"
+        Me.tn.Size = New System.Drawing.Size(100, 20)
+        Me.tn.TabIndex = 5
+        '
+        'tc
+        '
+        Me.tc.Location = New System.Drawing.Point(264, 119)
+        Me.tc.Name = "tc"
+        Me.tc.Size = New System.Drawing.Size(100, 20)
+        Me.tc.TabIndex = 6
+        '
+        'tx
+        '
+        Me.tx.Location = New System.Drawing.Point(264, 84)
+        Me.tx.Name = "tx"
+        Me.tx.Size = New System.Drawing.Size(100, 20)
+        Me.tx.TabIndex = 7
+        '
+        'tin
+        '
+        Me.tin.Location = New System.Drawing.Point(264, 235)
+        Me.tin.Name = "tin"
+        Me.tin.Size = New System.Drawing.Size(100, 20)
+        Me.tin.TabIndex = 8
+        '
+        'ibb
+        '
+        Me.ibb.Location = New System.Drawing.Point(264, 152)
+        Me.ibb.Name = "ibb"
+        Me.ibb.Size = New System.Drawing.Size(100, 20)
+        Me.ibb.TabIndex = 9
+        '
+        'Salida
+        '
+        Me.Salida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Salida.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column8, Me.Column9, Me.Column10, Me.Column11})
+        Me.Salida.Location = New System.Drawing.Point(12, 333)
+        Me.Salida.Name = "Salida"
+        Me.Salida.Size = New System.Drawing.Size(543, 165)
+        Me.Salida.TabIndex = 10
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Indice"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "x(i)"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "y(i)deltak(0)"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "deltak 1"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "deltk 2"
+        Me.Column5.Name = "Column5"
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "deltak 3"
+        Me.Column8.Name = "Column8"
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = "deltak 4"
+        Me.Column9.Name = "Column9"
+        '
+        'Column10
+        '
+        Me.Column10.HeaderText = "deltak 5"
+        Me.Column10.Name = "Column10"
+        '
+        'Column11
+        '
+        Me.Column11.HeaderText = "deltak 6"
+        Me.Column11.Name = "Column11"
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(596, 12)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(300, 300)
+        Me.Chart1.TabIndex = 11
+        Me.Chart1.Text = "Chart1"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column6, Me.Column7})
+        Me.DataGridView2.Location = New System.Drawing.Point(617, 347)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(242, 151)
+        Me.DataGridView2.TabIndex = 12
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Coeficientes s"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Deltas k"
+        Me.Column7.Name = "Column7"
+        '
+        'Calcular
+        '
+        Me.Calcular.Location = New System.Drawing.Point(419, 43)
+        Me.Calcular.Name = "Calcular"
+        Me.Calcular.Size = New System.Drawing.Size(75, 23)
+        Me.Calcular.TabIndex = 13
+        Me.Calcular.Text = "Calcular"
+        Me.Calcular.UseVisualStyleBackColor = True
+        '
+        'Graficar
+        '
+        Me.Graficar.Location = New System.Drawing.Point(419, 93)
+        Me.Graficar.Name = "Graficar"
+        Me.Graficar.Size = New System.Drawing.Size(75, 23)
+        Me.Graficar.TabIndex = 14
+        Me.Graficar.Text = "Graficar"
+        Me.Graficar.UseVisualStyleBackColor = True
+        '
+        'Limpiar
+        '
+        Me.Limpiar.Location = New System.Drawing.Point(419, 147)
+        Me.Limpiar.Name = "Limpiar"
+        Me.Limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Limpiar.TabIndex = 15
+        Me.Limpiar.Text = "Limpiar "
+        Me.Limpiar.UseVisualStyleBackColor = True
+        '
+        'Salir
+        '
+        Me.Salir.Location = New System.Drawing.Point(419, 191)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(75, 23)
+        Me.Salir.TabIndex = 16
+        Me.Salir.Text = "Salir"
+        Me.Salir.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(108, 198)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "S es igual a"
+        '
+        'ts
+        '
+        Me.ts.Location = New System.Drawing.Point(264, 191)
+        Me.ts.Name = "ts"
+        Me.ts.Size = New System.Drawing.Size(100, 20)
+        Me.ts.TabIndex = 18
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(108, 284)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "m es igual"
+        '
+        'tm
+        '
+        Me.tm.Location = New System.Drawing.Point(264, 281)
+        Me.tm.Name = "tm"
+        Me.tm.Size = New System.Drawing.Size(100, 20)
+        Me.tm.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(908, 538)
+        Me.Controls.Add(Me.tm)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ts)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.Limpiar)
+        Me.Controls.Add(Me.Graficar)
+        Me.Controls.Add(Me.Calcular)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.Salida)
+        Me.Controls.Add(Me.ibb)
+        Me.Controls.Add(Me.tin)
+        Me.Controls.Add(Me.tx)
+        Me.Controls.Add(Me.tc)
+        Me.Controls.Add(Me.tn)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Salida, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents tn As TextBox
+    Friend WithEvents tc As TextBox
+    Friend WithEvents tx As TextBox
+    Friend WithEvents tin As TextBox
+    Friend WithEvents ibb As TextBox
+    Friend WithEvents Salida As DataGridView
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Calcular As Button
+    Friend WithEvents Graficar As Button
+    Friend WithEvents Limpiar As Button
+    Friend WithEvents Salir As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ts As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+    Friend WithEvents Column10 As DataGridViewTextBoxColumn
+    Friend WithEvents Column11 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Label7 As Label
+    Friend WithEvents tm As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim i, c, redon, n, ib As Integer
+    Dim x(), y(), vx, vy, s, m As Single
+    Private Sub Salir_Click(sender As Object, e As EventArgs) Handles Salir.Click
+        End
+    End Sub
+
+
+
+
+    Private Sub Calcular_Click(sender As Object, e As EventArgs) Handles Calcular.Click
+        n = tn.Text
+        c = tc.Text
+        vx = tx.Text
+        ReDim x(0 To n)
+        ReDim y(0 To n)
+
+        For i = 0 To n - 1
+
+            x(i) = InputBox(" x(" & i & ")= ")
+            y(i) = InputBox(" y(" & i & ")= ")
+
+            Salida.Rows.Add(i, x(i), y(i))
+        Next
+
+
+        i = 0
+        Do While vx > x(i)
+            i = i + 1
+
+        Loop
+        ib = i - 1
+        ibb.Text = ib
+        s = (vx - x(ib)) / (x(ib + 1) - x(ib))
+        ts.Text = s
+        m = n - (ib + 1)
+        tm.Text = m
+
+    End Sub
+End Class
